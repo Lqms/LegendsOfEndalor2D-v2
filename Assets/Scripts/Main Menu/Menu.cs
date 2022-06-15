1f764e27fa40a44170ab7eb618aa1c713c62e1ad
Luqmus <rjcnz1007@yandex.ru>
2022-06-15 15:06:15 +0400

    MainMenu

---
+++ Assets/Scripts/Main Menu/Menu.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Menu : MonoBehaviour
+{
+    [SerializeField] private Button _continue;
+    [SerializeField] private Button _newGame;
+    [SerializeField] private Button _loadGame;
+    [SerializeField] private Button _settings;
+    [SerializeField] private Button _exit;
+
+    private void OnEnable()
+    {
+        _continue.onClick.AddListener(OnContinueButtonClick);
+        _newGame.onClick.AddListener(OnNewGameButtonClick);
+        _loadGame.onClick.AddListener(OnLoadGameButtonClick);
+        _settings.onClick.AddListener(OnSettingsButtonClick);
+        _exit.onClick.AddListener(OnExitButtonClick);
+    }
+
+    private void OnDisable()
+    {
+        _continue.onClick.RemoveListener(OnContinueButtonClick);
+        _newGame.onClick.RemoveListener(OnNewGameButtonClick);
+        _loadGame.onClick.RemoveListener(OnLoadGameButtonClick);
+        _settings.onClick.RemoveListener(OnSettingsButtonClick);
+        _exit.onClick.RemoveListener(OnExitButtonClick);
+    }
+
+    private void OnContinueButtonClick()
+    {
+        Debug.Log("Continue");
+    }
+
+    private void OnNewGameButtonClick()
+    {
+        Debug.Log("New game");
+    }
+
+    private void OnLoadGameButtonClick()
+    {
+        Debug.Log("Load game");
+    }
+
+    private void OnSettingsButtonClick()
+    {
+        Debug.Log("Settings");
+    }
+
+    private void OnExitButtonClick()
+    {
+        Debug.Log("Exit");
+        Application.Quit();
+    }
+}
--- Assets/Scripts/Main Menu/PressInfo.cs
+++ Assets/Scripts/Main Menu/PressInfo.cs
@@ -1,8 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
 
 public class PressInfo : MonoBehaviour
 {
-    
+    [SerializeField] private GameObject _text;
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        // _text.transform.DOScale(2, 3);
+    }
 }
