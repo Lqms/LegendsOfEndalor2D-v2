fd1a9961a2cf78f87e3b041357163b7636cfd07e
Luqmus <rjcnz1007@yandex.ru>
2022-07-15 17:51:14 +0400

    tutorial

--- Assets/Scripts/Player/PlayerMover.cs
+++ Assets/Scripts/Player/PlayerMover.cs
@@ -2,17 +2,64 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D), typeof(Animator), typeof(SpriteRenderer))]
 public class PlayerMover : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("Movement")]
+    [SerializeField] private float _speed;
+
+    [Header("Jump")]
+    [SerializeField] private float _jumpPower;
+    [SerializeField] private LayerMask _groundMask;
+    [SerializeField] private float _radiusLegs;
+    [SerializeField] private Transform _legs;
+    [SerializeField] private bool _canJump;
+
+    private Rigidbody2D _rigidBody;
+    private SpriteRenderer _spriteRenderer;
+    private Animator _animator;
+
+    private void Start()
     {
-        
+        _rigidBody = GetComponent<Rigidbody2D>();
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+        _animator = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        float inputHorizontal = Input.GetAxis("Horizontal");
+        Move(inputHorizontal);
+        _animator.SetBool("isRunning", inputHorizontal != 0);
+
+        if (inputHorizontal != 0)
+            FlipSpriteX(inputHorizontal < 0);
+
+
+        if (_canJump && Input.GetKeyDown(KeyCode.Space))
+        {
+            Jump();
+            _animator.SetTrigger("Jump");
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        _canJump = Physics2D.OverlapCircle(_legs.position, _radiusLegs, _groundMask);
+    }
+
+    private void Move(float direction)
+    {
+        _rigidBody.velocity = new Vector2(_speed * direction, _rigidBody.velocity.y);
+    }
+
+    private void FlipSpriteX(bool isFlipped)
+    {
+        _spriteRenderer.flipX = isFlipped;
+    }
+
+    private void Jump()
+    {
+        _rigidBody.AddForce(Vector2.up * _jumpPower, ForceMode2D.Impulse);
     }
 }
--- Assets/Scripts/Tutorial/CharacterCreator.cs
+++ Assets/Scripts/Tutorial/CharacterCreator.cs
@@ -9,10 +9,24 @@ public class CharacterCreator : MonoBehaviour, ISceneLoadHandler<Classes>
     [SerializeField] private GameObject[] _characterPrefabs;
     [SerializeField] private CinemachineVirtualCamera _virtualCamera;
 
+    [Header("For tests")]
+    [SerializeField] private GameObject _characterPrefab;
+    [SerializeField] private bool _isTest;
+
     public void OnSceneLoaded(Classes argument)
     {
-        GameObject character = Instantiate(_characterPrefabs[(int)argument], transform.position, Quaternion.identity);
+        GameObject character = Instantiate(_characterPrefabs[(int)argument], transform.position, Quaternion.identity);     
         _virtualCamera.Follow = character.transform;
         Destroy(gameObject);
+    }
+
+    private void Start()
+    {
+        if (_isTest)
+        {
+            GameObject character = Instantiate(_characterPrefab, transform.position, Quaternion.identity);
+            _virtualCamera.Follow = character.transform;
+            Destroy(gameObject);
+        }
     }
 }
